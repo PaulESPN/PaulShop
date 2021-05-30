819ac384f06ec62fcc3df52a3610454a4c40704e
Paul <espinozapauln@gmail.com>
2021-05-29 23:45:00 -0500

    Fixing wsgi file

--- backend/backend/wsgi.py
+++ backend/backend/wsgi.py
@@ -9,7 +9,12 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 
 import os
 
+
 from django.core.wsgi import get_wsgi_application
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(BASE_DIR)
+os.environ['DJANGO_SETTINGS_MODULE'] = 'backend.settings'
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
